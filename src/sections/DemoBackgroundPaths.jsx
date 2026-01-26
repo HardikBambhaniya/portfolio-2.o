def4195e6f1c9a4e1e4605a6eb6b98a89fcb7fb8
HardikBambhaniya <hardikb181@gmail.com>
2026-01-26 11:22:40 +0530

    add profilepic and update layout of webgl bg,  remove startborder com.

--- src/sections/DemoBackgroundPaths.jsx
+++ src/sections/DemoBackgroundPaths.jsx
@@ -3,7 +3,7 @@ import WebGLShader from '../components/web-gl-shader'
 
 export default function DemoOne() {
   return (
-    <div className="w-screen h-screen overflow-hidden absolute -z-10">
+    <div className="max-w-4xl mx-auto overflow-hidden absolute -z-10">
       <WebGLShader/> 
       
     </div>
