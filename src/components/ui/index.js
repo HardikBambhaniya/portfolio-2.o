15ea884b64c2222d76cb90066a54faf21d33eddc
HardikBambhaniya <hardikb181@gmail.com>
2026-01-27 14:59:46 +0530

    Add svg assests and file to manage icons import

---
+++ src/components/ui/index.js
@@ -0,0 +1,15 @@
+export { Button } from "./button";
+export { WhatsappIcon } from "./whatsapp-icon";
+export { GithubIcon } from "./github";
+export { LinkedinIcon } from "./linkedin";
+export { TwitterIcon } from "./twitter";
+export { FileTextIcon } from "./file-text";
+export { MailCheckIcon } from "./mail-check";
+
+export {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "./tooltip";
+export {default as SkillBadge} from "../SkillBadge";
