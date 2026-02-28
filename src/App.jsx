15bd4747cf7e2f6dcfed0186f89db3d6a4013646
HardikBambhaniya <hardikb181@gmail.com>
2026-02-28 11:30:08 +0530

    refactor: restructure routing and enhance loading experience with Suspense

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,30 @@
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
- import { Navbar } from "./components/Navbar";
+import { lazy, Suspense } from "react";
+import { Routes, Route} from "react-router-dom";
+import { Navbar } from "./components/Navbar";
 
 import LandingPage from "./page/LandingPage";
 import Aboutme from "./sections/Aboutme";
 import Projects from "./sections/Projects";
-// import Services from "./sections/Services";
-import ScrollToTop from "./components/common/ScrollToTop";
+
 
 
 
 function App() {
   return (
     <>
-    <ScrollToTop />
+    
       <Navbar/>
       
       
-      <Routes>
-        <Route path="/" element = {<>
-        <LandingPage />
-        </>} />
-        <Route path="/about" element = {<Aboutme/>} />
-        <Route path="/projects" element = {<Projects/>} />
-        {/* <Route path="/services" element = {<Services/>} /> */}
-      </Routes>
+      <Suspense fallback={<div className="text-white mt-20">Loading...</div>}>
+        
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/about" element={<Aboutme />} />
+            <Route path="/projects" element={<Projects />} />
+          </Routes>
+        
+      </Suspense>
     </>
   );
 }
