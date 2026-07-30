868a187baf564fe448a9581a09c89e3942013342
HardikBambhaniya <hardikb181@gmail.com>
2026-07-31 00:01:39 +0530

    feat: add Footer component and include it in the App layout

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+export const Footer = () => {
+
+return(<>
+
+<div className="pt-10 max-w-4xl mx-auto">
+ <p className="text-white text-center text-sm">© {new Date().getFullYear()} Hardik Bambhaniya  All rights reserved. </p>
+</div>
+
+</>)
+
+
+}
